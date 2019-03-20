9ad423f7dba8e150d3c87c6371f0d861dd540b33
Will Jarvis <will@toohotlimited.com>
2019-03-20 20:12:10 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,13 +2,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 
 function init() {
-  
   document.body.addEventListener('keydown', onKeyDownHandler)
 }
-
-
-// Key codes for A, B, and C keys.
-// const alphabet = [65, 66, 67];
  
 // Keep track of index outside of the event handler.
 let index = 0;
